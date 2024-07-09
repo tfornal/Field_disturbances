a9346cf813a4c3e91c632e1da22030b6983125dd
tfornal <tomasz.fornal6@gmail.com>
2024-07-09 13:51:12 +0200

    working sphere calculator, to be tested

--- calc_spheres/sphere_calc5.py
+++ calc_spheres/sphere_calc5.py
@@ -1,9 +1,11 @@
 import numpy as np
 from pathlib import Path
 import pyvista as pv
+from scipy.spatial import cKDTree
+from itertools import combinations
+import math
 
 
-##### DZIALAJACA TESTOWA WERSJA
 def add_edge_columns(matrix):
 
     last_column = matrix[:, -1]  ### to jest tez endpoint
@@ -46,11 +48,6 @@ def get_mid_points(indices_connections, matrix_extended):
     return mid_points
 
 
-##### DZIALAJACA TESTOWA WERSJA
-cwd = Path.cwd() / "calc_spheres/"
-data = np.loadtxt(cwd / "theta-phi-121-721_endpoint-True.txt", delimiter=",")
-
-
 def docelowe_dane():
     cols_nr_original = 121  ### w kierunku poloidalnym
     rows_nr_original = 721  ### w kierunku toroidalnym
@@ -89,31 +86,45 @@ def docelowe_dane():
 
     mid_points_dataset = np.array((mid_points_x, mid_points_y, mid_points_z)).T
 
-    ##### TRZEBA JESZCZE USUNAC DUPLIKATY
-    def plot():
-
-        fig = pv.Plotter()
-        fig.set_background("black")
-        x, y, z = data[:, 0], data[:, 1], data[:, 2]
-
-        # nx, ny = 121, 721  # Wymiary siatki
-        # x = x.reshape((nx, ny))
-        # y = y.reshape((nx, ny))
-        # z = z.reshape((nx, ny))
-
-        # grid = pv.StructuredGrid(x, y, z)
-        # fig.add_mesh(
-        #     grid,
-        #     opacity=0.1,
-        #     color="green",
-        # )
-        fig.add_mesh(data, render_points_as_spheres=True)
-        print(len(mid_points_dataset), data.shape)
-        fig.add_mesh(mid_points_dataset, color="red", render_points_as_spheres=True)
-        fig.show()
-
-    plot()
     return mid_points_dataset
+
+
+def find_nearest_neighbors(points1, points2, k=4):
+    # Stwórz drzewo KD z drugiej tablicy punktów
+    tree = cKDTree(points2)
+
+    # Znajdź k najbliższych sąsiadów dla każdego punktu z pierwszej tablicy
+    distances, indices = tree.query(points1, k=k)
+
+    # Zwróć listę k najbliższych punktów dla każdego punktu z points1
+    nearest_neighbors = points2[indices]
+    return nearest_neighbors
+
+
+def triangle_area_3d(A, B, C):
+    AB = B - A
+    AC = C - A
+    cross_product = np.cross(AB, AC)
+    area = 0.5 * np.linalg.norm(cross_product)
+
+    ##################TODO SPRAWDZIC!!!!!!!!!!!!!!!!!!!!!!!!
+    return area
+
+
+def plot():
+
+    fig = pv.Plotter()
+    fig.set_background("black")
+    x, y, z = data[:, 0], data[:, 1], data[:, 2]
+    fig.add_mesh(data, render_points_as_spheres=True)
+    fig.add_mesh(mid_points_dataset, color="red", render_points_as_spheres=True)
+    fig.add_mesh(
+        nearest_neighbors[0][:2],
+        color="purple",
+        point_size=15,
+        render_points_as_spheres=True,
+    )
+    fig.show()
 
 
 def test():
@@ -135,10 +146,68 @@ def test():
         indexes, indices_of_points_to_connect
     )
     mid_points = get_mid_points(indices_connections, matrix_extended)
-    print(len(mid_points), len(indices_connections))
+    # print(len(mid_points), len(indices_connections))
+
     return mid_points
 
 
+def distance(p1, p2):
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    d = math.sqrt(
+        math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2) + math.pow(z2 - z1, 2) * 1.0
+    )
+    return d
+
+
 if __name__ == "__main__":
-    # mid_points = test()
+    cwd = Path.cwd() / "calc_spheres/"
+    data = np.loadtxt(cwd / "theta-phi-121-721_endpoint-True.txt", delimiter=",")
+
     mid_points_dataset = docelowe_dane()
+    nearest_neighbors = find_nearest_neighbors(data, mid_points_dataset)
+    # wieksze_counter = 0
+    # mniejsze_counter = 0
+    spheres = []
+    for vertices in nearest_neighbors:
+        # vertices_combinations = list(combinations(i, 2))
+        # for idx in range(len(vertices) - 1):
+        dist_1 = distance(vertices[0], vertices[1])
+        dist_2 = distance(vertices[0], vertices[2])
+        dist_3 = distance(vertices[0], vertices[3])
+
+        distances = [dist_1, dist_2, dist_3]
+        max_value = max(distances)
+        max_index = distances.index(max_value)
+        points_left_indices = [1, 2, 3]
+        diagonal_point = points_left_indices.pop(max_index)
+        # diagonal_line = np.vstack(
+        #     (vertices[0].tolist(), vertices[diagonal_point].tolist())
+        # )
+        first_triangle_points = np.array(
+            (vertices[0], vertices[points_left_indices[0]], vertices[diagonal_point])
+        )
+        second_triangle_points = np.array(
+            (vertices[0], vertices[points_left_indices[1]], vertices[diagonal_point])
+        )
+        thickness = 4  # mm
+        surface_area = triangle_area_3d(*first_triangle_points) + triangle_area_3d(
+            *second_triangle_points
+        )
+        volume = float(surface_area * thickness)
+        spheres.append(volume)
+    data_with_spheres = np.concatenate((data, np.array(spheres).reshape(-1, 1)), axis=1)
+    points = data_with_spheres[:, :3]
+    diameters = data_with_spheres[:, 3]
+
+    plotter = pv.Plotter()
+    point_cloud = pv.PolyData(points)
+    point_cloud["diameter"] = diameters
+
+    spheres = point_cloud.glyph(scale="diameter", geom=pv.Sphere())
+    plotter.add_mesh(spheres, color="white", opacity=0.7)
+
+    plotter.show()
+
+    breakpoint()
+    plot()
